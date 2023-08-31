8d1159dd8c53c93c0b4f1b44b70dd8a8666b74a7
Alexey Guskov <alexey.guskov@ombori.com>
2023-08-31 16:42:43 +0200

    feat: add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import { Tree } from 'antd';
+import { Tree, Card, Space, Button } from 'antd';
 import type { DataNode, TreeProps } from 'antd/es/tree';
-
+import { PlusOutlined } from '@ant-design/icons';
 
 const treeData: DataNode[] = [
   {
@@ -33,6 +33,7 @@ const treeData: DataNode[] = [
   },
 ];
 
+
 function App() {
   const onSelect: TreeProps['onSelect'] = (selectedKeys, info) => {
     console.log('selected', selectedKeys, info);
@@ -43,16 +44,21 @@ function App() {
   };
 
   return (
-    <Tree
-      checkable
-      defaultExpandedKeys={['0-0-0', '0-0-1']}
-      defaultSelectedKeys={['0-0-0', '0-0-1']}
-      defaultCheckedKeys={['0-0-0', '0-0-1']}
-      onSelect={onSelect}
-      onCheck={onCheck}
-      treeData={treeData}
-      draggable
-    />
+    <Space style={{ width: '100vw', height: '100vh', alignItems: 'center', justifyContent: 'center' }} align="center" >
+      <Card title="Default size card" extra={"some text here"} style={{ width: 300 }}>
+        <Tree
+          checkable
+          defaultExpandedKeys={['0-0-0', '0-0-1']}
+          defaultSelectedKeys={['0-0-0', '0-0-1']}
+          defaultCheckedKeys={['0-0-0', '0-0-1']}
+          onSelect={onSelect}
+          onCheck={onCheck}
+          treeData={treeData}
+          draggable
+        />
+        <Button type="link" icon={<PlusOutlined />}>new entry</Button>
+      </Card>
+    </Space>
   );
 }
 

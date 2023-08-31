7dd562935e2387d4e403fcbf75a5adcabf8edc6b
Alexey Guskov <alexey.guskov@ombori.com>
2023-08-31 21:18:07 +0200

    feat: adding, editing items

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,6 @@ import { ChangeEvent, MouseEvent, useState } from "react";
 // TODO: publish app somewhere
 // TODO: tf for server
 
-// const treeData: DataNode[] = [
-//   {
-//     title: 'parent 1',
-//     key: '0-0',
-//     children: [
-//       {
-//         title: 'parent 1-0',
-//         key: '0-0-0',
-//         disabled: true,
-//         children: [
-//           {
-//             title: 'leaf',
-//             key: '0-0-0-0',
-//             disableCheckbox: true,
-//           },
-//           {
-//             title: 'leaf',
-//             key: '0-0-0-1',
-//           },
-//         ],
-//       },
-//       {
-//         title: 'parent 1-1',
-//         key: '0-0-1',
-//         children: [{ title: <span style={{ color: '#1677ff' }}>sss</span>, key: '0-0-1-0' }],
-//       },
-//     ],
-//   },
-// ];
-
 const id = () => Number(Math.random() * 0xffffffff).toString(16);
 
 function App() {

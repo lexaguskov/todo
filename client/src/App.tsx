27f3ec2e73bb6072728e1c0e6a3eb738b5bf0e1f
Alexey Guskov <alexey.guskov@ombori.com>
2023-09-01 15:13:32 +0200

    fix: collaborative editing wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Card, Space, Button, Input, Checkbox, Result, Tooltip } from "antd";
 import { PlusOutlined, CloseOutlined, DeleteOutlined } from "@ant-design/icons";
-import { KeyboardEvent, MouseEvent, FocusEvent } from "react";
+import { KeyboardEvent, FocusEvent, useEffect } from "react";
 import { usePersistedState } from "./usePersistedState";
 import { styled } from "styled-components";
 import io from "socket.io-client";
@@ -19,114 +19,146 @@ type Node = {
   checked: boolean;
 };
 
-const socket = io("http://localhost:3001");
+const socket = io("http://localhost:3001", {
+  transports: ["websocket"],
+});
 socket.on("connect", () => {
   console.log("connected");
 });
 socket.on("disconnect", () => {
   console.log("disconnected");
 });
-socket.on("message", (data) => {
-  console.log(data);
-});
-socket.on("error", (err) => {});
+socket.on("error", (err) => { });
 
 const id = () => Number(Math.random() * 0xffffffff).toString(16);
 
 function App() {
   const [lists, setLists] = usePersistedState<List[]>("lists", []);
 
-  const onCreateListClick = () => {
-    const newList = { title: "New todo list", key: id(), entries: [] };
+  const onCreateListClick = (listKey: string, emit: boolean = true) => {
+    const newList = { title: "New todo list", key: listKey, entries: [] };
+    emit && socket.emit("edit", { type: "list.create", listKey });
     setLists((lists) => [...lists, newList]);
   };
 
-  const onSetData = (list: List, data: Node[]) => {
-    setLists((lists) =>
-      lists.map((l) => (l === list ? { ...l, entries: data } : l)),
-    );
-  };
-
-  const onSetTitle = (list: List, title: string) => {
+  const onSetTitle = (listKey: string, title: string, emit: boolean = true) => {
     // if (title === '' && list.entries.length === 0) {
     //   setLists(lists => lists.filter(l => l !== list));
     //   return;
     // }
-    socket.emit("edit", { type: "list.title", title, key: list.key });
-    setLists((lists) => lists.map((l) => (l === list ? { ...l, title } : l)));
+    console.log('editing title for list', listKey, title)
+    emit && socket.emit("edit", { type: "list.title", title, listKey });
+    setLists((lists) => lists.map((l) => (l.key === listKey ? { ...l, title } : l)));
   };
 
-  const onDeleteListClick = (list: List) => {
-    socket.emit("edit", { type: "list.delete", key: list.key });
-    setLists((lists) => lists.filter((l) => l !== list));
+  const onDeleteListClick = (listKey: string, emit: boolean = true) => {
+    emit && socket.emit("edit", { type: "list.delete", listKey });
+    setLists((lists) => lists.filter((l) => l.key !== listKey));
   };
 
-  const onDeleteItem = (list: List, key: string) => {
-    socket.emit("edit", {
+  const onDeleteItem = (listKey: string, itemKey: string, emit: boolean = true) => {
+    emit && socket.emit("edit", {
       type: "list.item.delete",
-      key: list.key,
-      itemKey: key,
+      listKey: listKey,
+      itemKey: itemKey,
     });
     setLists((lists) =>
       lists.map((l) =>
-        l === list
-          ? { ...l, entries: l.entries.filter((e) => e.key !== key) }
+        l.key === listKey
+          ? { ...l, entries: l.entries.filter((e) => e.key !== itemKey) }
           : l,
       ),
     );
   };
 
-  const onCheck = (list: List, checked: boolean, key: string) => {
-    socket.emit("edit", {
+  const onCheck = (listKey: string, checked: boolean, itemKey: string, emit: boolean = true) => {
+    emit && socket.emit("edit", {
       type: "list.item.check",
-      key: list.key,
-      itemKey: key,
+      listKey,
+      itemKey,
       checked,
     });
     setLists((lists) =>
       lists.map((l) =>
-        l === list
+        l.key === listKey
           ? {
-              ...l,
-              entries: l.entries.map((e) =>
-                e.key === key ? { ...e, checked } : e,
-              ),
-            }
+            ...l,
+            entries: l.entries.map((e) =>
+              e.key === itemKey ? { ...e, checked } : e,
+            ),
+          }
           : l,
       ),
     );
   };
 
-  const onAddItem = (list: List) => {
-    socket.emit("edit", { type: "list.item.add", key: list.key });
-    const newItem: Node = { key: id(), title: "", checked: false };
+  const onAddItem = (listKey: string, itemKey: string, emit: boolean = true) => {
+    emit && socket.emit("edit", { type: "list.item.add", listKey, itemKey });
+    const newItem: Node = { key: itemKey, title: "", checked: false };
     setLists((lists) =>
       lists.map((l) =>
-        l === list ? { ...l, entries: [...l.entries, newItem] } : l,
+        l.key === listKey ? { ...l, entries: [...l.entries, newItem] } : l,
       ),
     );
   };
 
-  const onChangeItem = (list: List, val: string, key: string) => {
-    socket.emit("edit", {
+  const onChangeItem = (listKey: string, val: string, itemKey: string, emit: boolean = true) => {
+    emit && socket.emit("edit", {
       type: "list.item.title",
-      key: list.key,
-      itemKey: key,
+      listKey,
+      itemKey,
       title: val,
     });
     setLists((lists) =>
       lists.map((l) =>
-        l === list
+        l.key === listKey
           ? {
-              ...l,
-              entries: l.entries.map((e) =>
-                e.key === key ? { ...e, title: val } : e,
-              ),
-            }
+            ...l,
+            entries: l.entries.map((e) =>
+              e.key === itemKey ? { ...e, title: val } : e,
+            ),
+          }
           : l,
       ),
     );
   };
+
+  useEffect(() => {
+    socket.on("edit", (msg) => {
+      console.log('message', msg)
+      if (msg.type === "list.title") {
+        onSetTitle(msg.listKey, msg.title, false);
+      }
+
+      if (msg.type === "list.delete") {
+        onDeleteListClick(msg.listKey, false);
+      }
+
+      if (msg.type === "list.item.delete") {
+        onDeleteItem(msg.listKey, msg.itemKey, false);
+      }
+
+      if (msg.type === "list.item.check") {
+        onCheck(msg.listKey, msg.checked, msg.itemKey, false);
+      }
+
+      if (msg.type === "list.item.add") {
+        onAddItem(msg.listKey, msg.itemKey, false);
+      }
+
+      if (msg.type === "list.item.title") {
+        onChangeItem(msg.listKey, msg.title, msg.itemKey, false);
+      }
+
+      if (msg.type === "list.create") {
+        onCreateListClick(msg.listKey, false);
+      }
+    });
+
+    return () => {
+      socket.off("edit");
+    }
+  }, []);
 
   return (
     <Space
@@ -142,17 +174,17 @@ function App() {
       {lists.map((list) => (
         <TodoList
           key={list.key}
-          onDeleteListClick={() => onDeleteListClick(list)}
-          onChangeTitle={(val) => onSetTitle(list, val)}
+          onDeleteListClick={() => onDeleteListClick(list.key)}
+          onChangeTitle={(val) => onSetTitle(list.key, val)}
           title={list.title}
           data={list.entries}
-          onDeleteItem={(key) => onDeleteItem(list, key)}
-          onCheck={(val, key) => onCheck(list, val, key)}
-          onAddItem={() => onAddItem(list)}
-          onChangeItem={(val, key) => onChangeItem(list, val, key)} // TODO: debounce
+          onDeleteItem={(key) => onDeleteItem(list.key, key)}
+          onCheck={(val, key) => onCheck(list.key, val, key)}
+          onAddItem={() => onAddItem(list.key, id())}
+          onChangeItem={(val, key) => onChangeItem(list.key, val, key)} // TODO: debounce
         />
       ))}
-      <Card style={{ width: 300 }} hoverable onClick={onCreateListClick}>
+      <Card style={{ width: 300 }} hoverable onClick={() => onCreateListClick(id())}>
         <Result icon={<PlusOutlined />} title="Create a new list" />
       </Card>
     </Space>
@@ -297,7 +329,5 @@ const Row = styled.div`
     opacity: 1;
   }
 `;
-
-// socket.connect();
 
 export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -25,7 +25,15 @@ io.on("connection", (socket) => {
 
   socket.on('edit', (message) => {
     console.log('message', message);
-    io.emit('message', message); // Broadcast the message to all connected clients
+
+    // go thru list of socket.io sockets and send message to everyone except the sender
+    // socket.broadcast.emit('message', message);
+    for (const sock of io.sockets.sockets) {
+      if (sock[0] !== socket.id) {
+        sock[1].emit('edit', message);
+        console.log('sending message to', sock[0]);
+      }
+    }
   });
 
   socket.on('disconnect', () => {

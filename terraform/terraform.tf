0d6e844c71bcd29b88300d4f9693341be0c5005c
Alexey Guskov <alexey.guskov@ombori.com>
2023-09-07 10:40:49 +0200

    fix: cleanup

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,37 +1,8 @@
-# Specify the provider for GCP
 provider "google" {
   credentials = file("service-account-key.json")
   project     = "todo-397919"
   region      = "europe-north1"
 }
-
-# # Create a Google Cloud Storage Bucket
-# resource "google_storage_bucket" "static_website_bucket" {
-#   name     = "lexaguskov-todo-app"
-#   location = "EUROPE-NORTH1" # Use uppercase for the region
-
-#   # Enable website hosting
-#   website {
-#     main_page_suffix = "index.html"
-#     not_found_page   = "404.html"
-#   }
-# }
-
-# # Upload your static website files to the bucket
-# resource "google_storage_bucket_object" "static_website_files" {
-#   for_each = fileset("../frontend/build", "**/*")
-
-#   name   = each.value
-#   source = "../frontend/build/${each.value}"
-#   bucket = google_storage_bucket.static_website_bucket.name
-# }
-
-# resource "google_storage_bucket_iam_binding" "public_access" {
-#   bucket = google_storage_bucket.static_website_bucket.name
-#   role   = "roles/storage.objectViewer"
-
-#   members = ["allUsers"]
-# }
 
 resource "null_resource" "create_zip" {
   triggers = {
@@ -112,14 +83,6 @@ resource "google_dns_managed_zone" "guskov_dev" {
   description = "My DNS zone"
 }
 
-# resource "google_dns_record_set" "todo_guskov_dev" {
-#   name         = "todo.${google_dns_managed_zone.guskov_dev.dns_name}"
-#   type         = "CNAME"
-#   ttl          = 300
-#   managed_zone = google_dns_managed_zone.guskov_dev.name
-#   rrdatas      = ["${google_storage_bucket.static_website_bucket.name}.storage.googleapis.com"]
-# }
-
 resource "google_dns_record_set" "api_guskov_dev" {
   name         = "api.${google_dns_managed_zone.guskov_dev.dns_name}"
   type         = "A"
@@ -127,14 +90,6 @@ resource "google_dns_record_set" "api_guskov_dev" {
   managed_zone = google_dns_managed_zone.guskov_dev.name
   rrdatas      = [google_compute_instance.backend_instance.network_interface[0].access_config[0].nat_ip]
 }
-
-# output "website_url" {
-#   value = "https://todo.guskov.dev"
-# }
-
-# output "website_url" {
-#   value = "https://${google_storage_bucket.static_website_bucket.name}.storage.googleapis.com"
-# }
 
 output "instance_ip" {
   value = google_compute_instance.backend_instance.network_interface[0].access_config[0].nat_ip
